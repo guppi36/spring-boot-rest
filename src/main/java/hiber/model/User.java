d85401a39b8a761f6ea35ad8dffced4546f80466
guppi36 <sunoflindaine@gmail.com>
2021-03-18 14:08:38 +0300

    task complete4

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -33,7 +33,7 @@ public class User implements UserDetails {
     @Column(name = "age")
     private Integer age;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
             joinColumns = {@JoinColumn(name = "user_id")},
             inverseJoinColumns = @JoinColumn(name = "role_id"))
