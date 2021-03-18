75d54b5ac81f40bdbb0f0ade3f4bb4b1887eb72f
guppi36 <sunoflindaine@gmail.com>
2021-03-18 22:16:43 +0300

    task complete5

--- src/main/java/hiber/repository/UserRepository.java
+++ src/main/java/hiber/repository/UserRepository.java
@@ -2,10 +2,14 @@ package hiber.repository;
 
 import hiber.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByFirstName(String firstName);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = ?1")
     User findByEmail(String email);
 }
